19bf2899fe398897849df8eec5066e6414744176
Robinkumar2415 <159575571+Robinkumar2415@users.noreply.github.com>
2024-02-26 21:05:10 +0530

    Add files via upload

---
+++ 2assigment.cpp
@@ -0,0 +1,12 @@
+#include<iostream>
+using namespace std;
+int main(){
+    int x;
+    cout<<"enter first number\n";
+    cin>>x;
+    int y;
+    cout<<"enter second number\n";
+    cin>>y;
+    cout<<(x!=y) <<" "<< (x>=y);
+
+}
